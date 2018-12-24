fc9a39f8eb2854878cad5673025b8eedb158f45a
futurerahul <rah.chauhan1@gmail.com>
2018-12-24 16:38:45 +0530

    New commit-printlevel,height,diameter,mirror,findnode

--- BinaryTree/src/b_tree/Binarytreeuse.java
+++ BinaryTree/src/b_tree/Binarytreeuse.java
@@ -8,7 +8,7 @@ public class Binarytreeuse {
 	{
 		Scanner s=new Scanner(System.in);
 		int rootdata;
-		System.out.println("Enter the data: ");
+		System.out.println("Enter the root data: ");
 		rootdata=s.nextInt();
 		if(rootdata==-1)
 		{
@@ -66,6 +66,33 @@ public class Binarytreeuse {
 		print(root.right);
 	}
 	
+	public static void printlevel(Binarytreenode<Integer> root)
+	{
+		QueueLL<Binarytreenode<Integer>> q=new QueueLL<>();
+		q.enqueue(root);
+		while(!q.isEmpty())
+		{
+			try {
+				Binarytreenode<Integer> current=q.dequeue();
+				System.out.print(current.data+":");
+				if(current.left!=null)
+				{
+					q.enqueue(current.left);
+					System.out.print("L"+current.left.data);
+				}
+				if(current.right!=null)
+				{
+					q.enqueue(current.right);
+					System.out.print(",R"+current.right.data);
+				}
+				System.out.println();
+			} catch (QueueEmptyException1 e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
 	public static Binarytreenode<Integer> takeinput(Scanner s)
 	{
 		int rootdata;
@@ -80,6 +107,85 @@ public class Binarytreeuse {
 		root.right=takeinput(s);
 		return root;
 	}
+	
+	public static int height(Binarytreenode<Integer> root)
+	{
+		if(root==null)
+		{
+			return 0;
+		}
+		int curr_height=1;
+		int l_height=height(root.left);
+		int r_height=height(root.right);
+		curr_height+=Math.max(l_height, r_height);
+		return curr_height;
+	}
+	
+	public static int diameter(Binarytreenode<Integer> root)
+	{
+		if(root==null)
+		{
+			return 0;
+		}
+		int c_dia=height(root.left)+height(root.right);
+		int l_dia=diameter(root.left);
+		int r_dia=diameter(root.right);
+		int result=Math.max(c_dia,Math.max(l_dia,r_dia));
+		return result;
+	}
+	
+	public static Pair<Integer,Integer> heightDiameter (Binarytreenode<Integer> root)
+	{
+		if(root==null)
+		{
+			Pair<Integer,Integer> output=new Pair<>();
+			output.height=0;
+			output.diameter=0;
+			return output;
+		}
+		
+		Pair<Integer,Integer> lhd=heightDiameter(root.left);
+		Pair<Integer,Integer> rhd=heightDiameter(root.right);
+		int height=1+Math.max(lhd.height, rhd.height);
+		int op1=lhd.height+rhd.height;
+		int op2=lhd.diameter;
+		int op3=rhd.diameter;
+		int diameter=Math.max(op1, Math.max(op2, op3));
+		Pair<Integer,Integer> output=new Pair<>();
+		output.height=height;
+		output.diameter=diameter;
+		return output;
+		
+	}
+	
+	public static void mirror(Binarytreenode<Integer> root)
+	{
+		if(root==null){
+			return;
+		}
+		Binarytreenode<Integer> temp=root.left;
+		root.left=root.right;
+		root.right=temp;
+		
+		mirror(root.left);
+		mirror(root.right);
+	}
+	
+	public static boolean findNode(Binarytreenode<Integer> root,int x)
+	{
+		if(root==null)
+		{
+			return false;
+		}
+		boolean ans=false;
+		if(root.data==x)
+		{
+			ans=true;
+		}
+		boolean l_ans=findNode(root.left,x);
+		boolean r_ans=findNode(root.right,x);
+		return (ans||(l_ans||r_ans));
+	}
 
 	public static void main(String[] args) {
 		
@@ -88,6 +194,9 @@ public class Binarytreeuse {
 	//	print(root);
 		Binarytreenode<Integer> root=inputLevel();
 		print(root);
+		printlevel(root);
+		System.out.println("Diameter of tree: "+ diameter(root));
+		System.out.println("Diameter of tree: "+ heightDiameter(root).diameter+" Height of tree: "+ heightDiameter(root).height);
 
 	}
 
---
+++ BinaryTree/src/b_tree/Pair.java
@@ -0,0 +1,8 @@
+package b_tree;
+
+public class Pair<T,V> {
+	
+	public T height;
+	public V diameter;
+
+}
