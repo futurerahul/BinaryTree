1172d6d2634fb3d2f672ee70b6afb55cb9c013c6
futurerahul <rah.chauhan1@gmail.com>
2018-12-26 21:43:00 +0530

    New Commit+ in,pre,postorder,makeTree,Sum of nodes,isBalanced,level order traversal

--- BinaryTree/src/b_tree/Binarytreeuse.java
+++ BinaryTree/src/b_tree/Binarytreeuse.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Binarytreeuse {
 	
+	//take input levelwise
 	public static Binarytreenode<Integer> inputLevel()
 	{
 		Scanner s=new Scanner(System.in);
@@ -48,6 +49,7 @@ public class Binarytreeuse {
 		
 	}
 	
+	//print btree using recursion
 	public static void print(Binarytreenode<Integer> root)
 	{
 		if(root==null)
@@ -66,6 +68,7 @@ public class Binarytreeuse {
 		print(root.right);
 	}
 	
+	//print btree levelwise
 	public static void printlevel(Binarytreenode<Integer> root)
 	{
 		QueueLL<Binarytreenode<Integer>> q=new QueueLL<>();
@@ -93,6 +96,7 @@ public class Binarytreeuse {
 		}
 	}
 	
+	//take input using recursion for btree
 	public static Binarytreenode<Integer> takeinput(Scanner s)
 	{
 		int rootdata;
@@ -108,6 +112,7 @@ public class Binarytreeuse {
 		return root;
 	}
 	
+	//function to find height
 	public static int height(Binarytreenode<Integer> root)
 	{
 		if(root==null)
@@ -121,6 +126,7 @@ public class Binarytreeuse {
 		return curr_height;
 	}
 	
+	//function to fing diameter of btree
 	public static int diameter(Binarytreenode<Integer> root)
 	{
 		if(root==null)
@@ -134,6 +140,7 @@ public class Binarytreeuse {
 		return result;
 	}
 	
+	//better approach to find diameter of btree
 	public static Pair<Integer,Integer> heightDiameter (Binarytreenode<Integer> root)
 	{
 		if(root==null)
@@ -158,6 +165,7 @@ public class Binarytreeuse {
 		
 	}
 	
+	//function to mirror btree
 	public static void mirror(Binarytreenode<Integer> root)
 	{
 		if(root==null){
@@ -171,6 +179,7 @@ public class Binarytreeuse {
 		mirror(root.right);
 	}
 	
+	//function to check if node is present or not
 	public static boolean findNode(Binarytreenode<Integer> root,int x)
 	{
 		if(root==null)
@@ -186,17 +195,226 @@ public class Binarytreeuse {
 		boolean r_ans=findNode(root.right,x);
 		return (ans||(l_ans||r_ans));
 	}
+	
+	//inOder traversal of btree
+	public static void inOrder(Binarytreenode<Integer> root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		inOrder(root.left);
+		System.out.print(root.data+" ");
+		inOrder(root.right);
+	}
+	
+	//preOder traversal of btree
+		public static void preOrder(Binarytreenode<Integer> root)
+		{
+			if(root==null)
+			{
+				return;
+			}
+			System.out.print(root.data+" ");
+			preOrder(root.left);
+			preOrder(root.right);
+		}
+		
+		//postOder traversal of btree
+		public static void postOrder(Binarytreenode<Integer> root)
+		{
+			if(root==null)
+			{
+				return;
+			}
+			postOrder(root.left);
+			postOrder(root.right);
+			System.out.print(root.data+" ");
+		}
+		
+		
+	//make b-tree using inorder & preorder traversals 
+	public static Binarytreenode<Integer> premakeTree(int[] pre,int[] in,int preS,int preE,int inS,int inE)
+	  {
+	    
+	    if(inS>inE)
+	    {
+	      return null;
+	    }
+	    int rootdata=pre[preS];
+	    Binarytreenode<Integer> root=new Binarytreenode<>(rootdata);
+	    int i=inS;
+	    while(i<=inE)
+	    {
+	      if(in[i]==rootdata)
+	      {
+	        break;
+	      }
+	      i++;
+	    }
+	    
+	    int linS=inS;
+	    int linE=i-1;
+	    int lpreS=preS+1;
+	    int lpreE=preS+(linE-linS+1);
+	    
+	    int rinS=i+1;
+	    int rinE=inE;
+	    int rpreS=lpreE+1;
+	    int rpreE=preE;
+	    
+	    root.left=premakeTree(pre,in,lpreS,lpreE,linS,linE);
+	    root.right=premakeTree(pre,in,rpreS,rpreE,rinS,rinE);
+	    
+	    return root;
+	  }
+	  
+	//make b-tree using postorder and inorder traversals
+	public static Binarytreenode<Integer> postmakeTree(int[] post, int[] in,int postS, int postE,int inS,int inE)
+	  {
+	    if(inS>inE)
+	    {
+	      return null;
+	    }
+	    
+	    int rdata=post[postE];
+	    Binarytreenode<Integer> root=new Binarytreenode<>(rdata);
+	    int i=inS;
+	    while(i<=inE)
+	    {
+	      if(in[i]==rdata)
+	        break;
+	      i++;
+	    }
+	    
+	    int linS=inS;
+	    int linE=i-1;
+	    int lpostS=postS;
+	    int lpostE=postS+(linE-linS);;
+	    
+	    int rinS=i+1;
+	    int rinE=inE;
+	    int rpostS=lpostE+1;
+	    int rpostE=postE-1;
+	    
+	    root.left=postmakeTree(post,in,lpostS,lpostE,linS,linE);
+	    root.right=postmakeTree(post,in,rpostS,rpostE,rinS,rinE);
+	    
+	    return root;
+	  }
+	
+	//Sum of all nodes
+	public static int sum(Binarytreenode<Integer> root){
+      if(root==null)
+      {
+        return 0;
+      }
+      
+      int sum=root.data;
+      sum+=sum(root.left);
+      sum+=sum(root.right);
+      
+      return sum;
+	}
+	
+	//Check if btree is balanced
+	public static Pair2<Boolean,Integer> heightBalanced(Binarytreenode<Integer> root)
+	  {
+	    if(root==null)
+	    {
+	      Pair2<Boolean,Integer> output=new Pair2<>();
+	      output.isBalanced=true;
+	      output.height=0;
+	      return output;
+	    }
+	    
+	    Pair2<Boolean,Integer> lhB=heightBalanced(root.left);
+	    Pair2<Boolean,Integer> rhB=heightBalanced(root.right);
+	    int height=1+Math.max(lhB.height,rhB.height);
+	    if(Math.abs(lhB.height-rhB.height)>1)
+	    {
+	      Pair2<Boolean,Integer> output=new Pair2<>();
+	      output.isBalanced=false;
+	      output.height=height;
+	      return output;
+	    }
+	    
+	    boolean lans=lhB.isBalanced;
+	    boolean rans=rhB.isBalanced;
+	    
+	    boolean ans=lans&&rans;
+	    
+	    Pair2<Boolean,Integer> output=new Pair2<>();
+	      output.isBalanced=ans;
+	      output.height=height;
+	      return output;
+	  }
+	
+public static void printLevelWise(Binarytreenode<Integer> root){
+		
+		// Write your code here
+      
+      QueueLL<Binarytreenode<Integer>> pending=new QueueLL<>();
+      pending.enqueue(root);
+      pending.enqueue(null);
+      while(!pending.isEmpty())
+      {
+    	  try
+    	  {
+          Binarytreenode<Integer> current=pending.dequeue();
+          if(current!=null)
+          {
+            System.out.print(current.data+" ");
+            if(current.left!=null)
+            {
+            	pending.enqueue(current.left);
+            }
+            if(current.right!=null)
+            {
+            	pending.enqueue(current.right);
+            }
+          }
+          else
+          {
+           // Binarytreenode<Integer> delete=pending.dequeue();
+            System.out.println();
+            if(pending.size()!=0)
+            pending.enqueue(null);
+          }
+          
+          
+        }
+        catch(QueueEmptyException1 e)
+        {
+          //return null;
+        	e.printStackTrace();
+        }}
+      }
 
 	public static void main(String[] args) {
 		
-		//Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 		//Binarytreenode<Integer> root=takeinput(s);
-	//	print(root);
+	    //print(root);
 		Binarytreenode<Integer> root=inputLevel();
-		print(root);
-		printlevel(root);
-		System.out.println("Diameter of tree: "+ diameter(root));
-		System.out.println("Diameter of tree: "+ heightDiameter(root).diameter+" Height of tree: "+ heightDiameter(root).height);
+		printLevelWise(root);
+		//print(root);
+		//printlevel(root);
+		//System.out.println("Diameter of tree: "+ diameter(root));
+		//System.out.println("Diameter of tree: "+ heightDiameter(root).diameter+" Height of tree: "+ heightDiameter(root).height);
+		/*int size = s.nextInt();
+		int[] pre = new int[size];
+		for(int i = 0; i < size; i++){
+			pre[i] = s.nextInt();
+		}
+		int in[] = new int[size];
+		for(int i = 0; i < size; i++){
+			in[i] = s.nextInt();
+		}
+		Binarytreenode<Integer> root = premakeTree(pre,in,0,pre.length-1,0,in.length-1);
+		printlevel(root);*/
+		
+		
 
 	}
 
---
+++ BinaryTree/src/b_tree/Pair2.java
@@ -0,0 +1,8 @@
+package b_tree;
+
+public class Pair2<T,V> {
+	
+	public T isBalanced;
+	public V height;
+
+}
